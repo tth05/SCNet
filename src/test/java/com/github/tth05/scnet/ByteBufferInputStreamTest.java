f542ecbd5011dc4b581edb9ce387d00c2e3503da
tth05 <36999320+tth05@users.noreply.github.com>
2021-06-21 22:02:11 +0200

    Add tests for ByteBuffer streams

---
+++ src/test/java/com/github/tth05/scnet/ByteBufferInputStreamTest.java
@@ -0,0 +1,73 @@
+package com.github.tth05.scnet;
+
+import com.github.tth05.scnet.util.ByteBufferInputStream;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.nio.BufferUnderflowException;
+import java.nio.ByteBuffer;
+import java.nio.charset.StandardCharsets;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ByteBufferInputStreamTest {
+
+    private ByteBuffer buffer;
+    private ByteBufferInputStream stream;
+
+    @BeforeEach
+    public void setupStream() {
+        buffer = ByteBuffer.allocate(100);
+        stream = new ByteBufferInputStream(buffer);
+    }
+
+    @Test
+    public void testReadByte() {
+        buffer.put((byte) 25);
+        buffer.put((byte) -25);
+        buffer.flip();
+
+        assertEquals(25, stream.readByte());
+        assertEquals(-25, stream.readByte());
+        assertThrows(BufferUnderflowException.class, stream::readByte);
+    }
+
+    @Test
+    public void testReadByteArray() {
+        byte[] ar = new byte[]{1, 2, 3, 4, 5};
+        buffer.put(ar);
+        buffer.put(ar, 3, 2);
+        buffer.flip();
+
+        assertArrayEquals(new byte[]{1, 2, 3, 4, 5, 4, 5}, stream.readByteArray(7));
+        assertThrows(BufferUnderflowException.class, stream::readByte);
+    }
+
+    @Test
+    public void testReadShort() {
+        buffer.putShort(Short.MAX_VALUE);
+        buffer.flip();
+
+        assertEquals(Short.MAX_VALUE, stream.readShort());
+        assertThrows(BufferUnderflowException.class, stream::readByte);
+    }
+
+    @Test
+    public void testReadInt() {
+        buffer.putInt(Integer.MAX_VALUE);
+        buffer.flip();
+
+        assertEquals(Integer.MAX_VALUE, stream.readInt());
+        assertThrows(BufferUnderflowException.class, stream::readByte);
+    }
+
+    @Test
+    public void testReadString() {
+        buffer.putInt(14);
+        buffer.put("testReadString".getBytes(StandardCharsets.UTF_8));
+        buffer.flip();
+
+        assertEquals("testReadString", stream.readString());
+        assertThrows(BufferUnderflowException.class, buffer::get);
+    }
+}
---
+++ src/test/java/com/github/tth05/scnet/ByteBufferOutputStreamTest.java
@@ -0,0 +1,99 @@
+package com.github.tth05.scnet;
+
+import com.github.tth05.scnet.util.ByteBufferOutputStream;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.nio.BufferUnderflowException;
+import java.nio.ByteBuffer;
+import java.nio.charset.StandardCharsets;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ByteBufferOutputStreamTest {
+
+    private ByteBufferOutputStream stream;
+
+    @BeforeEach
+    public void setupStream() {
+        stream = new ByteBufferOutputStream(10);
+    }
+
+    @Test
+    public void testWriteByte() {
+        stream.writeByte(25);
+        stream.writeByte(-25);
+
+        ByteBuffer buffer = stream.getBuffer();
+        buffer.flip();
+        assertEquals(25, buffer.get());
+        assertEquals(-25, buffer.get());
+        assertThrows(BufferUnderflowException.class, buffer::get);
+    }
+
+    @Test
+    public void testAutoIncreaseBufferSize() {
+        ByteBuffer prevBuffer = stream.getBuffer();
+
+        assertEquals(10, stream.getBuffer().capacity());
+        for (int i = 0; i < 15; i++) {
+            stream.writeByte(12);
+        }
+
+        ByteBuffer buffer = stream.getBuffer();
+        assertNotSame(prevBuffer, buffer);
+        assertTrue(stream.getBuffer().capacity() >= 15);
+        buffer.flip();
+        for (int i = 0; i < 15; i++) {
+            assertEquals(12, buffer.get());
+        }
+        assertThrows(BufferUnderflowException.class, buffer::get);
+    }
+
+    @Test
+    public void testWriteByteArray() {
+        byte[] ar = new byte[]{1, 2, 3, 4, 5};
+        stream.writeByteArray(ar);
+        stream.writeByteArray(ar, 3, 2);
+
+        ByteBuffer buffer = stream.getBuffer();
+        buffer.flip();
+        byte[] dst = new byte[7];
+        buffer.get(dst);
+        assertArrayEquals(new byte[]{1, 2, 3, 4, 5, 4, 5}, dst);
+        assertThrows(BufferUnderflowException.class, buffer::get);
+    }
+
+    @Test
+    public void testWriteShort() {
+        stream.writeShort(Short.MAX_VALUE);
+
+        ByteBuffer buffer = stream.getBuffer();
+        buffer.flip();
+        assertEquals(Short.MAX_VALUE, buffer.getShort());
+        assertThrows(BufferUnderflowException.class, buffer::get);
+    }
+
+    @Test
+    public void testWriteInt() {
+        stream.writeInt(Integer.MAX_VALUE);
+
+        ByteBuffer buffer = stream.getBuffer();
+        buffer.flip();
+        assertEquals(Integer.MAX_VALUE, buffer.getInt());
+        assertThrows(BufferUnderflowException.class, buffer::get);
+    }
+
+    @Test
+    public void testWriteString() {
+        stream.writeString("testWriteString");
+
+        ByteBuffer buffer = stream.getBuffer();
+        buffer.flip();
+        assertEquals("testWriteString".getBytes(StandardCharsets.UTF_8).length, buffer.getInt());
+        byte[] dst = new byte["testWriteString".getBytes(StandardCharsets.UTF_8).length];
+        buffer.get(dst);
+        assertEquals("testWriteString", new String(dst, StandardCharsets.UTF_8));
+        assertThrows(BufferUnderflowException.class, buffer::get);
+    }
+}
